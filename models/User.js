d177b5c47374214a6434a44c09d827dfc4e22c88
Tarun Kumar Bhardwaj <95850621+itarun1@users.noreply.github.com>
2024-09-18 07:14:18 +0000

    user schema changes

--- models/User.js
+++ models/User.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
+  profile_pic: { type: String, required: false },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  subscriptionStatus: { type: String, default: 'inactive' },
+  jwt_token: { type: String, required: true },
+  mobile: { type: String, required: false },
+  address:{type:String,required:false},
+  user_status:{type:String,default: 'active' },
+  subscription_status: { type: String, default: 'inactive' },
   properties: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }],
+
 }, { timestamps: true });
 
 // Hash password before saving
